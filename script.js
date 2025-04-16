8b55d8638adb6aba3ba858aa907aedefbad36b64
Arpan Nyati <nyati.arpan@gmail.com>
2025-04-16 22:52:28 +0530

    .

--- script.js
+++
@@ -1,6 +0,0 @@
-const aboutMoreBtn = document.getElementById("about-more");
-
-aboutMoreBtn.addEventListener("click", function() {
-  // Add your logic here to display more information about yourself on click
-  alert("This is a placeholder for more information about you!");
-});
